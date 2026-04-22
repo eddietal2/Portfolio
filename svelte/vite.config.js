6792fb32b43a95dfe9e8a6b5d5e4fb28d617e291
Eddie Taliaferro <eddielacrosse2@gmail.com>
2026-04-22 11:27:49 -0400

    Vercel Build Error fix

--- svelte/vite.config.js
+++ svelte/vite.config.js
@@ -5,7 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    outDir: '../',
+    outDir: 'dist',
     rollupOptions: {
       output: {
         manualChunks: {
